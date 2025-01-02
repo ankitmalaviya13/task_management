0796de48d19af6a05130d3c27cf36c8894cc83ff
Kaushal Dasadiya <kaushal.dasadiya.kd@gmail.com>
2025-01-02 10:58:58 +0530

    Navigate To BottomBarScreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:task_management/screens/loginscreen.dart';
+import 'package:task_management/screens/bottomBar_Screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const LoginScreen(),
+      home: BottomBarScreen(),
     );
   }
 }
