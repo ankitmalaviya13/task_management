0ca2e560ca704b183e51a0d417cc98f501e292bf
Ankit <amalaviya@pragetx.com>
2025-01-03 15:56:13 +0530

    Device Info Package Add

---
+++ lib/core/DeviceInfo/device_information_model.dart
@@ -0,0 +1,20 @@
+class DeviceInformationModel {
+  String? deviceId;
+  String? deviceOs;
+  String? model;
+  String? appVersion;
+  String? brandName;
+  String? manufacturer;
+  String? osVersion;
+
+  DeviceInformationModel({
+    this.deviceId,
+    this.deviceOs,
+    this.model,
+    this.appVersion,
+    this.brandName,
+    this.manufacturer,
+    this.osVersion,
+  });
+
+}
