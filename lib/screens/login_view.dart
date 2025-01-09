7279e1fee5d4b4c18d9f7605d9e00155f3b26a84
Ankit <amalaviya@pragetx.com>
2025-01-09 14:51:28 +0530

    LoginView Api Call Ui Screen Loader Add

--- lib/screens/login_view.dart
+++ lib/screens/login_view.dart
@@ -13,158 +13,170 @@ class LoginView extends GetView<LoginSignupController> {
   @override
   Widget build(BuildContext context) {
     final isDarkMode = Theme.of(context).brightness == Brightness.dark;
-    return Stack(
-      children: [
-        GetBuilder<LoginSignupController>(
-            init: LoginSignupController(),
-            builder: (con) {
-              return Scaffold(
-                body: Center(
-                  child: Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: Form(
-                      key: controller.loginFormKey,
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          const Spacer(),
-                          const Row(
-                            children: [
-                              CommonText(
-                                text: "Welcome Back",
-                                fontSize: 22,
-                                fontWeight: FontWeight.w700,
-                              ),
-                            ],
-                          ),
-                          const Row(
-                            children: [
-                              CommonText(
-                                text: "to",
-                                fontSize: 22,
-                                fontWeight: FontWeight.w700,
-                              ),
-                              SizedBox(width: 10),
-                              CommonText(
-                                text: "CANGLY",
-                                fontSize: 22,
-                                fontWeight: FontWeight.w700,
-                                color: AppColor.primaryColor,
-                              ),
-                            ],
-                          ),
-                          const SizedBox(height: 10),
-                          const CommonText(
-                            text: "Hello there login to continue",
-                            color: AppColor.textColor,
-                          ),
-                          const SizedBox(height: 30),
-                          CommonTextFieldBorder(
-                              con: controller.emailController,
-                              hintText: "Email Address",
-                              hintTextColor: AppColor.primaryColor,
-                              borderColor: AppColor.primaryColor,
-                              validator: (value) {
-                                if (value!.isEmpty) {
-                                  return "Email is required";
-                                }
-                                String pattern = r'^[^@]+@[^@]+\.[^@]+';
-                                RegExp regex = RegExp(pattern);
-                                if (!regex.hasMatch(value)) {
-                                  return "Please enter a valid email address";
-                                }
-                                return null;
-                              }),
-                          const SizedBox(height: 20),
-                          Obx(() {
-                            return CommonTextFieldBorder(
-                              con: controller.passwordController,
-                              hintText: "Password",
-                              hintTextColor: AppColor.primaryColor,
-                              borderColor: AppColor.primaryColor,
-                              obscureText: controller.obSecurePassword.value,
-                              suffix: InkWell(
-                                onTap: () => controller.obSecurePassword.value = !controller.obSecurePassword.value,
-                                child: Icon(
-                                    controller.obSecurePassword.value ? Icons.visibility_off_rounded : Icons.visibility,
-                                    color: AppColor.darkGrey),
-                              ),
-                              validator: (v) {
-                                if (v!.trim().isEmpty) {
-                                  return "Password is required";
-                                }
-                                return null;
-                              },
-                            );
-                          }),
-                          const SizedBox(height: 15),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.end,
-                            children: [
-                              InkWell(
-                                onTap: () {
-                                  controller.onTapWelcomeResetPassword();
-                                },
-                                child: const CommonText(
-                                  text: "Forgot Password ?",
+    return PopScope(
+      canPop: false,
+      onPopInvoked: (pop) {
+        print("Fsdhkfsdkjfhskfsd");
+        if (pop) {
+          return;
+        }
+        controller.onTapLoginBack();
+      },
+      child: Stack(
+        children: [
+          GetBuilder<LoginSignupController>(
+              init: LoginSignupController(),
+              builder: (con) {
+                return Scaffold(
+                  body: Center(
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Form(
+                        key: controller.loginFormKey,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            const Spacer(),
+                            const Row(
+                              children: [
+                                CommonText(
+                                  text: "Welcome Back",
+                                  fontSize: 22,
+                                  fontWeight: FontWeight.w700,
+                                ),
+                              ],
+                            ),
+                            const Row(
+                              children: [
+                                CommonText(
+                                  text: "to",
+                                  fontSize: 22,
+                                  fontWeight: FontWeight.w700,
+                                ),
+                                SizedBox(width: 10),
+                                CommonText(
+                                  text: "CANGLY",
+                                  fontSize: 22,
+                                  fontWeight: FontWeight.w700,
                                   color: AppColor.primaryColor,
                                 ),
-                              )
-                            ],
-                          ),
-                          const SizedBox(height: 20),
-                          CommonButton(
-                            label: "Login",
-                            labelColor: AppColor.white,
-                            labelSize: 17,
-                            onPressed: () {
-                              controller.onTapLogin();
-                              controller.onTapKeyboard(context);
-                            },
-                          ),
-                          const Spacer(),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              RichText(
-                                textAlign: TextAlign.center,
-                                text: TextSpan(
-                                  text: "Didn't have account ? ",
-                                  style: TextStyle(
-                                    color: (isDarkMode ? Colors.white : Colors.black),
-                                    fontWeight: FontWeight.w400,
-                                    fontSize: 17,
-                                  ),
+                              ],
+                            ),
+                            const SizedBox(height: 10),
+                            const CommonText(
+                              text: "Hello there login to continue",
+                              color: AppColor.textColor,
+                            ),
+                            const SizedBox(height: 30),
+                            CommonTextFieldBorder(
+                                con: controller.emailController,
+                                hintText: "Email Address",
+                                hintTextColor: AppColor.primaryColor,
+                                borderColor: AppColor.primaryColor,
+                                validator: (value) {
+                                  if (value!.isEmpty) {
+                                    return "Email is required";
+                                  }
+                                  String pattern = r'^[^@]+@[^@]+\.[^@]+';
+                                  RegExp regex = RegExp(pattern);
+                                  if (!regex.hasMatch(value)) {
+                                    return "Please enter a valid email address";
+                                  }
+                                  return null;
+                                }),
+                            const SizedBox(height: 20),
+                            Obx(() {
+                              return CommonTextFieldBorder(
+                                con: controller.passwordController,
+                                hintText: "Password",
+                                hintTextColor: AppColor.primaryColor,
+                                borderColor: AppColor.primaryColor,
+                                obscureText: controller.obSecurePassword.value,
+                                suffix: InkWell(
+                                  onTap: () => controller.obSecurePassword.value = !controller.obSecurePassword.value,
+                                  child: Icon(
+                                      controller.obSecurePassword.value
+                                          ? Icons.visibility_off_rounded
+                                          : Icons.visibility,
+                                      color: AppColor.darkGrey),
                                 ),
-                              ),
-                              GestureDetector(
-                                onTap: () {
-                                  controller.onTapWelcomeSignup();
+                                validator: (v) {
+                                  if (v!.trim().isEmpty) {
+                                    return "Password is required";
+                                  }
+                                  return null;
                                 },
-                                child: RichText(
+                              );
+                            }),
+                            const SizedBox(height: 15),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              children: [
+                                InkWell(
+                                  onTap: () {
+                                    controller.onTapWelcomeResetPassword();
+                                  },
+                                  child: const CommonText(
+                                    text: "Forgot Password ?",
+                                    color: AppColor.primaryColor,
+                                  ),
+                                )
+                              ],
+                            ),
+                            const SizedBox(height: 20),
+                            CommonButton(
+                              label: "Login",
+                              labelColor: AppColor.white,
+                              labelSize: 17,
+                              onPressed: () {
+                                controller.onTapLogin();
+                                controller.onTapKeyboard(context);
+                              },
+                            ),
+                            const Spacer(),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                RichText(
                                   textAlign: TextAlign.center,
-                                  text: const TextSpan(
-                                    text: "Register",
+                                  text: TextSpan(
+                                    text: "Didn't have account ? ",
                                     style: TextStyle(
-                                      color: AppColor.primaryColor,
+                                      color: (isDarkMode ? Colors.white : Colors.black),
                                       fontWeight: FontWeight.w400,
                                       fontSize: 17,
                                     ),
                                   ),
                                 ),
-                              ),
-                            ],
-                          ),
-                          const SizedBox(height: 15),
-                        ],
+                                GestureDetector(
+                                  onTap: () {
+                                    controller.onTapWelcomeSignup();
+                                  },
+                                  child: RichText(
+                                    textAlign: TextAlign.center,
+                                    text: const TextSpan(
+                                      text: "Register",
+                                      style: TextStyle(
+                                        color: AppColor.primaryColor,
+                                        fontWeight: FontWeight.w400,
+                                        fontSize: 17,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                            const SizedBox(height: 15),
+                          ],
+                        ),
                       ),
                     ),
                   ),
-                ),
-              );
-            }),
-        Obx(() => controller.isLoginLoading.value ? customIndicator() : Container()),
-      ],
+                );
+              }),
+          Obx(() => controller.isLoginLoading.value ? customIndicator() : Container()),
+        ],
+      ),
     );
   }
 }
