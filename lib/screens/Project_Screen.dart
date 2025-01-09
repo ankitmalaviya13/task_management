b9ded8f6317d3d4d6a4d90bfb5e07007c61c9473
Ankit <amalaviya@pragetx.com>
2025-01-09 16:39:10 +0530

    Project Screen Add

---
+++ lib/screens/Project_Screen.dart
@@ -0,0 +1,328 @@
+import 'package:flutter/material.dart';
+import 'package:task_management/core/color/color.dart';
+import 'package:task_management/screens/widgets/common_button.dart';
+import 'package:task_management/screens/widgets/common_text.dart';
+
+class ProjectScreen extends StatelessWidget {
+  const ProjectScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        leading: InkWell(
+          onTap: () => Navigator.pop(context),
+          child: const Icon(
+            Icons.arrow_back_ios_new_rounded,
+          ),
+        ),
+        title: const CommonText(
+          text: "Pintap Project",
+          fontSize: 17,
+          fontWeight: FontWeight.w500,
+        ),
+        actions: const [
+          Icon(
+            Icons.star,
+            color: Colors.yellow,
+          ),
+          SizedBox(width: 5),
+          Icon(Icons.more_vert_outlined),
+          SizedBox(width: 15),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 5.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const CommonText(
+              text: "Create 50 Screens for mobile application",
+              fontSize: 15,
+            ),
+            const SizedBox(height: 22),
+            Column(
+              children: [
+                Row(
+                  children: [
+                    const Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          children: [
+                            Icon(Icons.person_add_alt_1, size: 16),
+                            SizedBox(width: 8),
+                            CommonText(
+                              text: "Team",
+                              fontSize: 13,
+                            ),
+                          ],
+                        ),
+                        SizedBox(height: 22),
+                        Row(
+                          children: [
+                            Icon(Icons.person, size: 16),
+                            SizedBox(width: 8),
+                            CommonText(
+                              text: "PM",
+                              fontSize: 13,
+                            ),
+                          ],
+                        ),
+                        SizedBox(height: 22),
+                        Row(
+                          children: [
+                            Icon(Icons.check_box_outlined, size: 16),
+                            SizedBox(width: 8),
+                            CommonText(
+                              text: "Status",
+                              fontSize: 13,
+                            ),
+                          ],
+                        ),
+                        SizedBox(height: 22),
+                        Row(
+                          children: [
+                            Icon(Icons.calendar_month, size: 16),
+                            SizedBox(width: 8),
+                            CommonText(
+                              text: "Due Date",
+                              fontSize: 13,
+                            ),
+                          ],
+                        ),
+                        SizedBox(height: 22),
+                        Row(
+                          children: [
+                            Icon(Icons.file_copy, size: 16),
+                            SizedBox(width: 8),
+                            CommonText(
+                              text: "File Attach",
+                              fontSize: 13,
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                    const SizedBox(width: 16),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Row(
+                          children: [
+                            Stack(
+                              children: [
+                                CircleAvatar(
+                                  backgroundColor: Colors.green,
+                                  radius: 10,
+                                ),
+                                Padding(
+                                  padding: EdgeInsets.only(left: 10),
+                                  child: CircleAvatar(
+                                    backgroundColor: Colors.yellow,
+                                    radius: 10,
+                                  ),
+                                ),
+                                Padding(
+                                  padding: EdgeInsets.only(left: 15),
+                                  child: CircleAvatar(
+                                    backgroundColor: AppColor.primaryColor,
+                                    radius: 10,
+                                  ),
+                                ),
+                                Padding(
+                                  padding: EdgeInsets.only(left: 20),
+                                  child: CircleAvatar(
+                                    backgroundColor: Colors.red,
+                                    radius: 10,
+                                  ),
+                                ),
+                                Padding(
+                                  padding: EdgeInsets.only(left: 25),
+                                  child: CircleAvatar(
+                                    radius: 10,
+                                    backgroundColor: Colors.white,
+                                    child: Text(
+                                      '+',
+                                      style: TextStyle(
+                                        color: Colors.blue,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                        const SizedBox(height: 20),
+                        const Row(
+                          children: [
+                            CircleAvatar(
+                              backgroundColor: Colors.green,
+                              radius: 10,
+                            ),
+                            SizedBox(width: 6),
+                            CommonText(
+                              text: "John Stemberger",
+                              fontSize: 11,
+                            ),
+                          ],
+                        ),
+                        const SizedBox(height: 20),
+                        Row(
+                          children: [
+                            CommonButton(
+                              width: 50,
+                              height: 22,
+                              label: "To Do",
+                              labelSize: 10, // Adjusted label size
+                              labelColor: AppColor.white,
+                              onPressed: () {},
+                            ),
+                          ],
+                        ),
+                        const SizedBox(height: 20),
+                        const Row(
+                          children: [
+                            CommonText(
+                              text: "17 Jan, 2023",
+                              fontSize: 11,
+                            ),
+                          ],
+                        ),
+                        const SizedBox(height: 20),
+                        Row(
+                          children: [
+                            CommonButton(
+                              width: 50,
+                              height: 22,
+                              label: "+ Add",
+                              labelSize: 10,
+                              labelColor: AppColor.white,
+                              onPressed: () {},
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            const SizedBox(height: 20),
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 10.0),
+              child: CommonButton(
+                buttonRadius: 2.0,
+                label: "Add Property",
+                labelSize: 17,
+                labelColor: AppColor.primaryColor,
+              ),
+            ),
+            const SizedBox(height: 20),
+            const CommonText(
+              text: "Sub Task",
+              fontSize: 17,
+              fontWeight: FontWeight.w500,
+            ),
+            const SizedBox(height: 10),
+            Expanded(
+              child: ListView.builder(
+                scrollDirection: Axis.vertical,
+                itemCount: 10,
+                itemBuilder: (context, index) {
+                  return Container(
+                    margin: const EdgeInsets.symmetric(horizontal: 5.0, vertical: 5.0),
+                    padding: const EdgeInsets.all(8.0),
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      color: AppColor.white,
+                      borderRadius: BorderRadius.circular(10),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.2),
+                          spreadRadius: 2,
+                          blurRadius: 5,
+                          offset: const Offset(0, 3),
+                        ),
+                      ],
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          children: [
+                            const CommonText(
+                              text: 'Client Review & Feedback',
+                              fontSize: 15,
+                              color: AppColor.black,
+                              fontWeight: FontWeight.w500,
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                            const Spacer(),
+                            Checkbox(
+                              value: false,
+                              onChanged: (value) {},
+                            ),
+                          ],
+                        ),
+                        const CommonText(
+                          text: 'Indeep Project Website',
+                          color: AppColor.greyText,
+                          overflow: TextOverflow.ellipsis,
+                          fontSize: 13,
+                        ),
+                        const SizedBox(height: 10),
+                        const CommonText(
+                          text: 'Today 10:00PM - 10:30 PM',
+                          color: AppColor.black,
+                          fontSize: 15,
+                        ),
+                      ],
+                    ),
+                  );
+                },
+              ),
+            ),
+            const SizedBox(height: 10),
+            Container(
+              padding: const EdgeInsets.all(8.0),
+              decoration: BoxDecoration(
+                color: AppColor.white,
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.2),
+                    spreadRadius: 2,
+                    blurRadius: 5,
+                    offset: const Offset(0, 3),
+                  ),
+                ],
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Row(
+                children: [
+                  const Expanded(
+                    child: TextField(
+                      decoration: InputDecoration(
+                        hintText: "Post a comment",
+                        hintStyle: TextStyle(color: Colors.grey),
+                        border: InputBorder.none,
+                      ),
+                    ),
+                  ),
+                  IconButton(
+                    icon: const Icon(Icons.send, color: AppColor.primaryColor),
+                    onPressed: () {},
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 10),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/screens/home_Screen.dart
+++ lib/screens/home_Screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:task_management/screens/Project_Screen.dart';
 import 'package:task_management/screens/myProject_Screen.dart';
 import 'package:task_management/screens/notification_screen.dart';
 import 'package:task_management/screens/today_Task.dart';
@@ -106,125 +107,134 @@ class HomeScreen extends StatelessWidget {
                 scrollDirection: Axis.horizontal,
                 itemCount: 10,
                 itemBuilder: (context, index) {
-                  return Container(
-                    padding: const EdgeInsets.only(left: 5),
-                    margin: const EdgeInsets.only(right: 10),
-                    width: MediaQuery.of(context).size.width * 0.5,
-                    decoration: const BoxDecoration(color: AppColor.primaryColor),
-                    child: const Padding(
-                      padding: EdgeInsets.all(8.0),
-                      child: Column(
-                        children: [
-                          Row(
-                            children: [
-                              Expanded(
-                                child: CommonText(
-                                  text: 'Pintap Project',
-                                  fontSize: 14,
-                                  color: Colors.white,
-                                  fontWeight: FontWeight.w500,
-                                  overflow: TextOverflow.ellipsis,
+                  return InkWell(
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => ProjectScreen(),
+                          ));
+                    },
+                    child: Container(
+                      padding: const EdgeInsets.only(left: 5),
+                      margin: const EdgeInsets.only(right: 10),
+                      width: MediaQuery.of(context).size.width * 0.5,
+                      decoration: const BoxDecoration(color: AppColor.primaryColor),
+                      child: const Padding(
+                        padding: EdgeInsets.all(8.0),
+                        child: Column(
+                          children: [
+                            Row(
+                              children: [
+                                Expanded(
+                                  child: CommonText(
+                                    text: 'Pintap Project',
+                                    fontSize: 14,
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.w500,
+                                    overflow: TextOverflow.ellipsis,
+                                  ),
                                 ),
-                              ),
-                              Icon(
-                                Icons.more_vert,
-                                color: AppColor.white,
-                              )
-                            ],
-                          ),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.start,
-                            children: [
-                              Expanded(
-                                child: CommonText(
-                                  text: 'Wednesday 30 NOV, 2022',
-                                  color: Colors.white,
-                                  overflow: TextOverflow.ellipsis,
-                                  fontSize: 12,
+                                Icon(
+                                  Icons.more_vert,
+                                  color: AppColor.white,
+                                )
+                              ],
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: [
+                                Expanded(
+                                  child: CommonText(
+                                    text: 'Wednesday 30 NOV, 2022',
+                                    color: Colors.white,
+                                    overflow: TextOverflow.ellipsis,
+                                    fontSize: 12,
+                                  ),
                                 ),
-                              ),
-                            ],
-                          ),
-                          SizedBox(height: 20),
-                          Row(
-                            children: [
-                              Stack(
-                                children: [
-                                  CircleAvatar(
-                                    backgroundColor: Colors.green,
-                                    radius: 20,
-                                  ),
-                                  Padding(
-                                    padding: EdgeInsets.only(left: 10),
-                                    child: CircleAvatar(
-                                      backgroundColor: Colors.yellow,
+                              ],
+                            ),
+                            SizedBox(height: 20),
+                            Row(
+                              children: [
+                                Stack(
+                                  children: [
+                                    CircleAvatar(
+                                      backgroundColor: Colors.green,
                                       radius: 20,
                                     ),
-                                  ),
-                                  Padding(
-                                    padding: EdgeInsets.only(left: 15),
-                                    child: CircleAvatar(
-                                      backgroundColor: AppColor.primaryColor,
-                                      radius: 20,
+                                    Padding(
+                                      padding: EdgeInsets.only(left: 10),
+                                      child: CircleAvatar(
+                                        backgroundColor: Colors.yellow,
+                                        radius: 20,
+                                      ),
                                     ),
-                                  ),
-                                  Padding(
-                                    padding: EdgeInsets.only(left: 20),
-                                    child: CircleAvatar(
-                                      backgroundColor: Colors.red,
-                                      radius: 20,
+                                    Padding(
+                                      padding: EdgeInsets.only(left: 15),
+                                      child: CircleAvatar(
+                                        backgroundColor: AppColor.primaryColor,
+                                        radius: 20,
+                                      ),
                                     ),
-                                  ),
-                                  Padding(
-                                    padding: EdgeInsets.only(left: 25),
-                                    child: CircleAvatar(
-                                      radius: 20,
-                                      backgroundColor: Colors.white,
-                                      child: Text(
-                                        '+3',
-                                        style: TextStyle(
-                                          color: Colors.blue,
-                                          fontWeight: FontWeight.bold,
+                                    Padding(
+                                      padding: EdgeInsets.only(left: 20),
+                                      child: CircleAvatar(
+                                        backgroundColor: Colors.red,
+                                        radius: 20,
+                                      ),
+                                    ),
+                                    Padding(
+                                      padding: EdgeInsets.only(left: 25),
+                                      child: CircleAvatar(
+                                        radius: 20,
+                                        backgroundColor: Colors.white,
+                                        child: Text(
+                                          '+3',
+                                          style: TextStyle(
+                                            color: Colors.blue,
+                                            fontWeight: FontWeight.bold,
+                                          ),
                                         ),
                                       ),
                                     ),
+                                  ],
+                                ),
+                              ],
+                            ),
+                            SizedBox(height: 20),
+                            Row(
+                              children: [
+                                Expanded(
+                                  child: CommonText(
+                                    text: 'Website | Mobile App Design',
+                                    color: Colors.white,
+                                    fontSize: 12,
+                                    overflow: TextOverflow.ellipsis,
                                   ),
-                                ],
-                              ),
-                            ],
-                          ),
-                          SizedBox(height: 20),
-                          Row(
-                            children: [
-                              Expanded(
-                                child: CommonText(
-                                  text: 'Website | Mobile App Design',
-                                  color: Colors.white,
-                                  fontSize: 12,
-                                  overflow: TextOverflow.ellipsis,
                                 ),
-                              ),
-                            ],
-                          ),
-                          SizedBox(height: 15),
-                          Row(
-                            children: [
-                              Icon(
-                                Icons.access_time_outlined,
-                                color: AppColor.white,
-                                size: 18,
-                              ),
-                              SizedBox(width: 5),
-                              Expanded(
-                                child: CommonText(
-                                  text: "4 Days ago",
+                              ],
+                            ),
+                            SizedBox(height: 15),
+                            Row(
+                              children: [
+                                Icon(
+                                  Icons.access_time_outlined,
                                   color: AppColor.white,
-                                  overflow: TextOverflow.ellipsis,
+                                  size: 18,
                                 ),
-                              ),
-                            ],
-                          ),
-                        ],
+                                SizedBox(width: 5),
+                                Expanded(
+                                  child: CommonText(
+                                    text: "4 Days ago",
+                                    color: AppColor.white,
+                                    overflow: TextOverflow.ellipsis,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
                       ),
                     ),
                   );
